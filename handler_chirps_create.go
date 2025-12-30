32bed19a6d87b007f79f79b0b171711051fcafb5
its-PKN-2k4 <phuckhanhnguyen2004@gmail.com>
2025-12-29 23:56:11 -0500

    Refactor code into separate files for singleton functionalities and add endpoint for chirp creation

---
+++ handler_chirps_create.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+
+	"example.com/m/internal/database"
+	"github.com/google/uuid"
+)
+
+type Chirp struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	UserID    uuid.UUID `json:"user_id"`
+	Body      string    `json:"body"`
+}
+
+func (cfg *apiConfig) handlerChirpsCreate(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body   string    `json:"body"`
+		UserID uuid.UUID `json:"user_id"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	log.Printf("Decoded params: body=%q user_id=%s", params.Body, params.UserID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
+		return
+	}
+
+	cleaned, err := validateChirp(params.Body)
+	log.Printf("Decoded cleaned: body=%q", cleaned)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, err.Error(), err)
+		return
+	}
+
+	chirp, err := cfg.db.CreateChirp(r.Context(), database.CreateChirpParams{
+		Body:   cleaned,
+		UserID: params.UserID,
+	})
+	log.Printf("Decoded chirp: body=%q user_id=%s", chirp.Body, chirp.UserID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create chirp", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, Chirp{
+		ID:        chirp.ID,
+		CreatedAt: chirp.CreatedAt,
+		UpdatedAt: chirp.UpdatedAt,
+		Body:      chirp.Body,
+		UserID:    chirp.UserID,
+	})
+}
+
+func validateChirp(body string) (string, error) {
+	const maxChirpLength = 140
+	if len(body) > maxChirpLength {
+		return "", errors.New("Chirp is too long")
+	}
+
+	badWords := map[string]struct{}{
+		"kerfuffle": {},
+		"sharbert":  {},
+		"fornax":    {},
+	}
+	cleaned := getCleanedBody(body, badWords)
+	return cleaned, nil
+}
+
+func getCleanedBody(body string, badWords map[string]struct{}) string {
+	words := strings.Split(body, " ")
+	for i, word := range words {
+		loweredWord := strings.ToLower(word)
+		if _, ok := badWords[loweredWord]; ok {
+			words[i] = "****"
+		}
+	}
+	cleaned := strings.Join(words, " ")
+	return cleaned
+}
--- server.go
+++ server.go
@@ -2,198 +2,66 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"strings"
 	"sync/atomic"
 
-	"github.com/joho/godotenv"
-
-	"time"
-
 	"example.com/m/internal/database"
-	"github.com/google/uuid"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
-// Struct to store stateful (counting requests to an endpoint)
 type apiConfig struct {
 	fileserverHits atomic.Int32
 	db             *database.Queries
 	platform       string
 }
 
-// Middleware to alter/record state and process requests
-func (cfg *apiConfig) middlewareMetricsIncrement(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
-
-		next.ServeHTTP(w, r)
-	})
-}
-
-// Function to handle POST request and return stats as json
-func JsonHandler(w http.ResponseWriter, r *http.Request) {
-	type parameters struct {
-		Body string `json:"body"`
-	}
-
-	decoder := json.NewDecoder(r.Body)
-	params := parameters{}
-	err := decoder.Decode(&params)
-	if err != nil {
-		log.Printf("Error decoding parameters: %s", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	type returnVals struct {
-		Cleaned_body string `json:"cleaned_body"`
-	}
-
-	resContent := returnVals{}
-
-	w.Header().Set("Content-Type", "application/json")
-	if len(params.Body) <= 140 {
-		to_filter := []string{"kerfuffle", "sharbert", "fornax"}
-		string_splits := strings.Split(params.Body, " ")
-		for i := 0; i < len(string_splits); i++ {
-			for j := 0; j < len(to_filter); j++ {
-				if strings.ToLower(string_splits[i]) == to_filter[j] {
-					string_splits[i] = "****"
-				}
-			}
-		}
-		resContent.Cleaned_body = strings.Join(string_splits, " ")
-		w.WriteHeader(http.StatusOK)
-	} else {
-		resContent.Cleaned_body = ""
-		w.WriteHeader(http.StatusBadRequest)
-	}
-	data, err := json.Marshal(resContent)
-	if err != nil {
-		log.Printf("Error marshalling JSON: %s", err)
-		w.WriteHeader((http.StatusInternalServerError))
-		return
-	}
-	w.Write(data)
-
-}
-
-func (cfg *apiConfig) handlerUsersCreate(w http.ResponseWriter, r *http.Request) {
-	type User struct {
-		ID        uuid.UUID `json:"id"`
-		CreatedAt time.Time `json:"created_at"`
-		UpdatedAt time.Time `json:"updated_at"`
-		Email     string    `json:"email"`
-	}
-
-	type RegisterEmail struct {
-		Email string `json:"email"`
-	}
-
-	decoder := json.NewDecoder(r.Body)
-	regEmail := RegisterEmail{}
-	err := decoder.Decode(&regEmail)
-	if err != nil {
-		log.Printf("Error decoding parameters: %s", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	user, err := cfg.db.CreateUser(r.Context(), regEmail.Email)
-	newUser := User{user.ID, user.CreatedAt, user.UpdatedAt, user.Email}
-	w.WriteHeader(http.StatusCreated)
-	data, err := json.Marshal(newUser)
-	if err != nil {
-		log.Printf("Error marshalling JSON: %s", err)
-		w.WriteHeader((http.StatusInternalServerError))
-		return
-	}
-	w.Write(data)
-}
-
-func (cfg *apiConfig) handleResetUsers(w http.ResponseWriter, r *http.Request) {
-	if cfg.platform != "dev" {
-		w.WriteHeader(http.StatusForbidden)
-	}
-
-	if r.Method != "POST" {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-	}
-
-	err := cfg.db.EmptyUsers(r.Context())
-	if err != nil {
-		log.Printf("Something is wrong... Please wait for a fix")
-	}
-	w.WriteHeader(http.StatusOK)
-}
-
 func main() {
-	err1 := godotenv.Load()
-	if err1 != nil {
-		log.Printf("Error loading godotenv lib: %s", err1)
-	}
-	dbURL := os.Getenv("DB_URL")
-	db, err2 := sql.Open("postgres", dbURL)
-	if err2 != nil {
-		log.Printf("Error opening connection to SQL database: %s", err2)
-	}
-	dbQueries := database.New(db)
+	const filepathRoot = "."
+	const port = "8080"
 
-	mux := http.NewServeMux()
-	apiCFG := apiConfig{
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL must be set")
+	}
+	platform := os.Getenv("PLATFORM")
+	if platform == "" {
+		log.Fatal("PLATFORM must be set")
+	}
+
+	dbConn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Error opening database: %s", err)
+	}
+	dbQueries := database.New(dbConn)
+
+	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
 		db:             dbQueries,
-		platform:       os.Getenv("PLATFORM"),
+		platform:       platform,
 	}
-	// Instance of stateful struct
-	mux.Handle("/app/", apiCFG.middlewareMetricsIncrement(http.StripPrefix("/app", http.FileServer(http.Dir("."))))) // Handler for /app endpoint
 
-	// Handler for /api/healthz endpoint
-	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, req *http.Request) {
-		if req.Method != "GET" {
-			w.WriteHeader(http.StatusMethodNotAllowed)
-		}
+	mux := http.NewServeMux()
+	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/", fsHandler)
 
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
-	})
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
 
-	// Handler for /admin/metrics endpoint
-	mux.HandleFunc("GET /admin/metrics", func(w http.ResponseWriter, req *http.Request) {
-		if req.Method != "GET" {
-			w.WriteHeader(http.StatusMethodNotAllowed)
-		}
+	mux.HandleFunc("POST /api/users", apiCfg.handlerUsersCreate)
 
-		w.Header().Set("Content-Type", "text/html")
-		w.WriteHeader(http.StatusOK)
-		hitCount := fmt.Sprintf(`
-		<html>
-  			<body>
-    			<h1>Welcome, Chirpy Admin</h1>
-    			<p>Chirpy has been visited %d times!</p>
-  			</body>
-		</html>`, apiCFG.fileserverHits.Load())
-		w.Write([]byte(hitCount))
-	})
+	mux.HandleFunc("POST /api/chirps", apiCfg.handlerChirpsCreate)
 
-	// Handler for /admin/reset endpoint
-	mux.HandleFunc("POST /admin/reset", apiCFG.handleResetUsers)
-
-	// Handler for /api/validate_chirp endpoint
-	mux.HandleFunc("POST /api/validate_chirp", JsonHandler)
-
-	// Handler for /api/users endpoint
-	mux.HandleFunc("POST /api/users", apiCFG.handlerUsersCreate)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 
 	srv := &http.Server{
-		Addr:    ":8080",
+		Addr:    ":" + port,
 		Handler: mux,
 	}
-	srv.ListenAndServe()
+
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(srv.ListenAndServe())
 }
