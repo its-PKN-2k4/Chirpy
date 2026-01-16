1407afbb8f43081feca3623146d36cca2c41eab4
its-PKN-2k4 <phuckhanhnguyen2004@gmail.com>
2026-01-15 22:55:07 -0500

    Add functions for creating and validating JWT token

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,7 +1,12 @@
 package auth
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/alexedwards/argon2id"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 )
 
 func HashPassword(password string) (string, error) {
@@ -18,4 +23,39 @@ func CheckPasswordHash(password, hash string) (bool, error) {
 		return false, err
 	}
 	return match, nil
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    "chirpy",
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn)),
+		Subject:   userID.String(),
+	})
+
+	ss, err := token.SignedString([]byte(tokenSecret))
+	return ss, err
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	claims := &jwt.RegisteredClaims{}
+	_, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(tokenSecret), nil
+	})
+
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("Error parsing token: %v", err)
+	}
+
+	idString := claims.Subject
+
+	realId, err := uuid.Parse(idString)
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("Error parsing result to id format: %v", err)
+	}
+	return realId, nil
 }
