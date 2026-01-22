8b3be350a979434a0a1aaa802603e404ba5b4ded
its-PKN-2k4 <phuckhanhnguyen2004@gmail.com>
2026-01-21 23:52:31 -0500

    Add new handler for processing membership update request

---
+++ handler_update_membership.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+
+	"net/http"
+
+	"github.com/google/uuid"
+)
+
+func (cfg *apiConfig) handlerUpdateMembership(w http.ResponseWriter, r *http.Request) {
+	type payload struct {
+		UserID string `json:"user_id"`
+	}
+
+	type parameters struct {
+		Event string  `json:"event"`
+		Data  payload `json:"data"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
+		return
+	}
+
+	if params.Event != "user.upgraded" {
+		respondWithJSON(w, http.StatusNoContent, nil)
+		return
+	}
+
+	idToUpgrade, err := uuid.Parse(params.Data.UserID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't extract user_id from request", err)
+		return
+	}
+
+	err1 := cfg.db.UpdateUserMembershipByID(r.Context(), idToUpgrade)
+	if err1 != nil {
+		respondWithError(w, http.StatusNotFound, "Couldn't find user with provided id", err)
+	}
+
+	respondWithJSON(w, http.StatusNoContent, nil)
+}
